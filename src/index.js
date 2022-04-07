96d8befb342dff9daefaeddd1533135444f40791
MisHkat <mishkat606@gmail.com>
2022-04-07 22:12:08 +0600

    m

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import 'tachyons';
-
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import { requestRobots, searchRobots } from './reducers'
